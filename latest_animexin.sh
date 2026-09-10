945121a7d5a30b9475cf1d638db65ba920559e9c
Aditya <adicse@live.com>
2026-09-10 11:18:30 +0000

    fix the script

--- latest_animexin.sh
+++ latest_animexin.sh
@@ -9,9 +9,12 @@ function get_axing_latest_episode() {
     local series_name="$1"
     local url="$2"
     local episode_regex="$3"
+
+    echo "getting page"
     local page="$(curl -ksL "${url}")"
 
     local latest_episode="$(echo "${page}" | grep -m 1 -P -o ${episode_regex})"
+    echo "latest_episode: ${latest_episode}"
 
     grep "${latest_episode}" latest_episodes.txt
     if [[ "$?" -eq 0 ]]; then
@@ -19,9 +22,14 @@ function get_axing_latest_episode() {
         return
     fi
 
-    sed -i "s${SED_DELIM}${series_name}:.*${SED_DELIM}${series_name}: ${latest_episode}${SED_DELIM}" latest_episodes.txt
+    if grep -qF "${series_name}:" latest_episodes.txt; then
+        sed -i "s${SED_DELIM}^${series_name}:.*${SED_DELIM}${series_name}: ${latest_episode}${SED_DELIM}" \
+            latest_episodes.txt
+    else
+        printf '%s: %s\n' "$series_name" "$latest_episode" >> latest_episodes.txt
+    fi
 
 }
 
 
-get_axing_latest_episode $swallowed_star_link $swallowed_star_regex
+get_axing_latest_episode "Swallowed_star" $swallowed_star_link $swallowed_star_regex
